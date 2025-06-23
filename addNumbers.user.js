e7bcc5058b84dc142693e6ed592bdf7911ec880c
valverde <manolo.valverde@gmail.com>
2025-06-23 23:14:24 +0900

    Correct origin of lists numbers.

--- addNumbers.user.js
+++ addNumbers.user.js
@@ -12,5 +12,5 @@ const stories_list = d.querySelector(".stories");
 stories_list.style.setProperty("list-style-type", "decimal");
 const start = Number(d.URL.match(/^https:\/\/lobste.rs\/page\/(\d+)\/?$/)?.[1] ?? null);
 if (start) {
-  stories_list.setAttribute("start", (start-1)*25);
+  stories_list.setAttribute("start", (start-1)*25 + 1);
 }
