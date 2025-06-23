3bd084181ed4e974da6909e8ddd97173c7cbbf44
valverde <manolo.valverde@gmail.com>
2025-06-23 22:39:22 +0900

    Add new script.

---
+++ addNumbers.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        addNumbers
+// @namespace   local
+// @description Add index numbers to article list
+// @match       https://lobste.rs/
+// @match       https://lobste.rs/page/*
+// ==/UserScript==
+
+const d = document;
+
+const stories_list = d.querySelector(".stories");
+stories_list.style.setProperty("list-style-type", "decimal");
+const start = Number(d.URL.match(/^https:\/\/lobste.rs\/page\/(\d+)\/?$/)?.[1] ?? null);
+if (start) {
+  stories_list.setAttribute("start", (start-1)*25);
+}
