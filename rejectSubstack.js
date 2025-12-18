3791bdbc2521cefab55aaa2db3ecf0708bac778a
valverde <manolo.valverde@gmail.com>
2025-12-18 19:01:50 +0900

    better js.

--- rejectSubstack.js
+++ rejectSubstack.js
@@ -6,6 +6,10 @@
 // @match     https://www.programmablemutter.com/*
 // ==/UserScript==
 
-Array.from(document.querySelectorAll("#main > div:nth-child(4) > div > div > div.pencraft.pc-display-flex.pc-gap-6.pc-mobile-gap-6.pc-reset > button:nth-child(2)")).
+const host = window.location.hostname.split(".").slice(-2).join(".");
+const query = {"substack.com":"#main > div:nth-child(4) > div > div > div.pencraft.pc-display-flex.pc-gap-6.pc-mobile-gap-6.pc-reset > button:nth-child(2)",
+               "programmablemutter.com":"button.priority_secondary-S63h9o:nth-child(2)" }[host]
+
+Array.from(document.querySelectorAll(query)).
     filter(element => element.textContent.trim() === "Reject").
     forEach(elt => elt.click());
