50060be7884fbbf33434bc0bfe1fad61b5ad6f17
valverde <manolo.valverde@gmail.com>
2026-04-11 19:56:29 +0900

    Make removeMDNad work with the new mozilla.org webpage.

--- removeMDNad.user.js
+++ removeMDNad.user.js
@@ -5,7 +5,17 @@
 // @match     https://*.mozilla.org/*
 // ==/UserScript==
 
-const tbd = document.querySelector(".top-banner");
-if (tbd?.querySelector("#pong-note")?.textContent?.trim() === "Ad") {
-    tbd.remove();
+//TODO: There are lots of shadow-roots involved so its difficult to find the element with "Ad" string.
+// This doesn't checks if the banners are real Ads or not but seems to work fine.
+
+const banner = document.querySelector(".page-layout__banner");
+const sub = banner?.querySelector("mdn-placement-top:nth-child(1)");
+if (sub) {
+    banner.remove();
 }
+
+const side_placement = document.querySelector(".layout__right-sidebar > mdn-placement-sidebar:nth-child(2)");
+side_placement?.remove();
+
+const bottom_placement = document.querySelector(".page-layout__footer > mdn-placement-bottom:nth-child(1)");
+bottom_placement?.remove();
